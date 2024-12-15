19e087cf2f2528c257be19ad7bc6f283e809a69b
Litorian113 <154338974+Litorian113@users.noreply.github.com>
2024-12-15 16:09:55 +0100

    change

--- shqipee/src/App.svelte
+++ shqipee/src/App.svelte
@@ -1,47 +1,38 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+    import { goto } from '$app/navigation'; // Gilt für SvelteKit
+
+let selected = 'elbasan';
+
+// Wenn sich der Wert ändert, wird die Navigation ausgeführt
+$: if (selected) {
+    goto(`/${selected}`);
+}
 </script>
 
-<main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/> 
+    <title>Elbasan Alphabet</title>
+</head>
+<body>
+  <header>
+   <div>
+    <h1>
+      elbasans alphabet
+    </h1>
+   </div> 
+   <div>
+    <select bind:value={selected}>
+      <option value="elbasan">elbasan</option>
+      <option value="vithkuqi">vithkuqi</option>
+  </select>
+   </div>
+</header>
+</body>
 
-  <div class="card">
-    <Counter />
-  </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+
+
 </style>
--- shqipee/svelte.config.js
+++ shqipee/svelte.config.js
@@ -1,4 +1,6 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { sveltekit } from '@sveltejs/kit/vite';
+
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
