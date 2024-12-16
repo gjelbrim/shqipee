00aacd1e33995552e7a29ee5144854db51f35e10
Gjelbrim Haskaj <g@gjelbr.im>
2024-12-16 18:13:48 +0100

    simpeled navbar

--- shqipee/src/components/Navbar.svelte
+++ shqipee/src/components/Navbar.svelte
@@ -1,40 +1,39 @@
 <script>
-    import { onMount } from "svelte";
-  
-    // Aktuelle Route
-    let activeRoute = window.location.hash || "#/";
-  
-    // Funktion, um die Route zu aktualisieren
-    const updateRoute = () => {
-      activeRoute = window.location.hash || "#/";
-    };
-  
-    // Event-Listener hinzufügen, um Änderungen der Route zu überwachen
-    onMount(() => {
-      window.addEventListener("hashchange", updateRoute);
-      updateRoute(); // Initiale Route setzen
-  
-      // Cleanup des Event-Listeners beim Verlassen der Komponente
-      return () => window.removeEventListener("hashchange", updateRoute);
-    });
-  </script>
-  
-  <nav class="navbar">
-    <a
-      href="#/"
-      class:active={activeRoute === "#/"}
-    >
-      elbasan
+  import { onMount } from "svelte";
+
+  // Aktuelle Route
+  let activeRoute = window.location.hash || "#/";
+
+  // Funktion, um die Route zu aktualisieren
+  const updateRoute = () => {
+    activeRoute = window.location.hash || "#/";
+  };
+
+  // Event-Listener hinzufügen, um Änderungen der Route zu überwachen
+  onMount(() => {
+    window.addEventListener("hashchange", updateRoute);
+    updateRoute(); // Initiale Route setzen
+
+    // Cleanup des Event-Listeners beim Verlassen der Komponente
+    return () => window.removeEventListener("hashchange", updateRoute);
+  });
+
+  // Define routes
+  const routes = [
+    { name: "elbasan", path: "#/" },
+    { name: "vithkuqi", path: "#/vithkuqi" },
+  ];
+</script>
+
+<nav class="navbar">
+  {#each routes as route}
+    <a href={route.path} class:active={activeRoute === route.path}>
+      {route.name}
     </a>
-    <a
-      href="#/vithkuqi"
-      class:active={activeRoute === "#/vithkuqi"}
-    >
-      vithkuqi
-    </a>
-  </nav>
-  
-  <style>
+  {/each}
+</nav>
+
+<style>
   .navbar {
     display: flex;
     justify-content: center;
@@ -43,18 +42,18 @@
     margin-bottom: 20px;
     background-color: #DBE1D5;
   }
-  
+
   .navbar a {
     text-decoration: none;
     font-size: 1.2rem;
     color: #333;
     position: relative; /* Für die Unterstreichung */
   }
-  
+
   .navbar a.active {
     color: #C3181E; /* Rot für aktive Seite */
   }
-  
+
   .navbar a.active::after {
     content: "";
     position: absolute;
@@ -64,5 +63,4 @@
     height: 2px;
     background-color: #C3181E; /* Unterstrich-Farbe */
   }
-  </style>
-  
+</style>
